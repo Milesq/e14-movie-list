5b8ff5815174fd7a67af94b2fa8cc6f85f83f87a
Milesq <wmilosz88@gmail.com>
2020-11-20 23:00:46 +0100

    test: add tests for creating movies

---
+++ packages/e14-backend/tests/save.spec.js
@@ -0,0 +1,60 @@
+const { gql } = require('apollo-server')
+const { createTestClient } = require('apollo-server-testing')
+const Movie = require('../src/model/movie')
+const server = require('../src/server')
+
+describe('Saving system', () => {
+  function createMovie(movie) {
+    const { mutate } = createTestClient(server)
+
+    const createMovie = gql`
+      mutation($movie: MovieInput!) {
+        createMovie(input: $movie) {
+          title
+        }
+      }
+    `
+
+    return mutate({
+      mutation: createMovie,
+      variables: { movie },
+    })
+  }
+
+  beforeEach(() => {
+    Movie.prototype.save = jest.fn()
+  })
+
+  it('works', async () => {
+    await createMovie({
+      genre: 'Sci-Fi',
+      year: 1999,
+      title: 'Star Wars: The Phantom Menace',
+      rating: 5,
+    })
+
+    expect(Movie.prototype.save).toBeCalled()
+  })
+
+  it('to reject old-age movies', async () => {
+    createMovie({
+      genre: 'Sci-Fi',
+      year: 20,
+      title: 'Star Wars: The Phantom Menace',
+      rating: 5,
+    })
+
+    expect(Movie.prototype.save).not.toBeCalled()
+  })
+
+  it('to reject movies with rating non-matching to [1, 5]', async () => {
+    await createMovie({
+      genre: 'Sci-Fi',
+      year: 1999,
+      title: 'Star Wars: The Phantom Menace',
+      rating: -1,
+    })
+
+    expect(Movie.prototype.save).not.toBeCalled()
+  })
+})
