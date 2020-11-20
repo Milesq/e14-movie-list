267c63073257c728bb3dcd2149a36fc35a20f6a7
Milesq <wmilosz88@gmail.com>
2020-11-20 20:11:20 +0100

    test: add tests for get

---
+++ packages/e14-backend/tests/get.spec.js
@@ -0,0 +1,34 @@
+const { default: mockingoose } = require('mockingoose')
+const { gql } = require('apollo-server')
+const { createTestClient } = require('apollo-server-testing')
+const server = require('../src/server')
+const Movie = require('../src/model/movie')
+
+describe('List getting', () => {
+  it('works', async () => {
+    const content = [
+      {
+        title: 'Revenge of the Sith',
+        genre: 'Sci-Fi',
+        productionYear: 2005,
+        rating: 5,
+      },
+    ]
+
+    mockingoose(Movie).toReturn(content)
+
+    const { query } = createTestClient(server)
+
+    const moviesQuery = gql`
+      query {
+        movie {
+          title
+        }
+      }
+    `
+
+    const res = await query({ query: moviesQuery })
+
+    expect(res.data.movie.title).toBe(res.body[0].title)
+  })
+})
