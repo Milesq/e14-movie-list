33e4bc52cc3fa30b35cd89b1eb48e43caa73e5db
Milesq <wmilosz88@gmail.com>
2020-11-21 01:36:32 +0100

    feat: add saving movies

--- packages/e14-backend/src/graphql/mutations/createMovie.js
+++ packages/e14-backend/src/graphql/mutations/createMovie.js
@@ -1,4 +1,6 @@
-const { gql } = require('apollo-server')
+const { gql, UserInputError } = require('apollo-server')
+const { default: ow } = require('ow')
+const Movie = require('../../model/movie')
 
 const input = gql`
   input MovieInput {
@@ -9,7 +11,30 @@ const input = gql`
   }
 `
 
-const resolver = () => {
+async function resolver(parent, args) {
+  const rules = ow.object.exactShape({
+    title: ow.string.minLength(1),
+    genre: ow.string,
+    rating: ow.number.inRange(1, 5).integer,
+    year: ow.number.greaterThanOrEqual(1900),
+  })
+
+  try {
+    ow(args.input, rules)
+  } catch (err) {
+    throw new UserInputError(err.message)
+  }
+
+  try {
+    await new Movie(args).save()
+  } catch (err) {
+    if (err.code !== 11000) throw err
+
+    throw new UserInputError(
+      'In our database exists the movie with the same title'
+    )
+  }
+
   return {
     title: 'Hello World',
   }
