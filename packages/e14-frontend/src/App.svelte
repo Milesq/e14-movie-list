13a78c847ef6d0f7053d9a3074d2a10b4413d0a9
Milesq <wmilosz88@gmail.com>
2020-11-21 23:38:12 +0100

    style: add and apply stylelint-config-idiomatic-order

--- packages/e14-frontend/src/App.svelte
+++ packages/e14-frontend/src/App.svelte
@@ -13,17 +13,17 @@
 
 <style lang="scss">
   main {
-    text-align: center;
-    padding: 1em;
     max-width: 240px;
+    padding: 1em;
     margin: 0 auto;
+    text-align: center;
   }
 
   h1 {
     color: #ff3e00;
-    text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+    text-transform: uppercase;
   }
 
   @media (min-width: 640px) {
--- packages/e14-frontend/stylelint.config.js
+++ packages/e14-frontend/stylelint.config.js
@@ -3,6 +3,7 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-prettier',
     'stylelint-scss',
+    'stylelint-config-idiomatic-order'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
