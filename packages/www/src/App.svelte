9947eb7a5b6f6ff7245f564c46482a1551129b57
Milesq <wmilosz88@gmail.com>
2020-11-23 02:15:08 +0100

    feat: pulse button

--- packages/www/src/App.svelte
+++ packages/www/src/App.svelte
@@ -1,12 +1,18 @@
 <main>
   <section class="section has-text-centered hero is-fullheight py-0 px-0">
-    <div class="hero-body is-flex is-flex-direction-column">
-        <h1 class="my-6">Filmoteka</h1>
-        <h2 class="subtitle is-capitalized is-size-4">Największa baza filmów!</h2>
+    <div style="position: relative" class="hero-body is-flex is-flex-direction-column">
+      <h1 class="my-6">Filmoteka</h1>
+      <h2 class="subtitle is-capitalized is-size-4">Największa baza filmów!</h2>
+
+      <div class="pulse-button__wrapper">
+        <PulseButton on:click={scroll} />
+      </div>
     </div>
   </section>
 
-  <MovieOptions />
+  <div bind:this={scrollTarget}>
+    <MovieOptions />
+  </div>
 
   <footer class="footer">
     <div class="content has-text-centered">
@@ -19,6 +25,17 @@
 
 <script>
   import MovieOptions from './components/MovieOptions.svelte'
+  import PulseButton from './components/PulseButton.svelte'
+  let scrollTarget
+
+  function scroll() {
+    const { y } = scrollTarget.getBoundingClientRect()
+
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth',
+    });
+  }
 </script>
 
 <style lang="scss">
@@ -42,5 +59,10 @@
     font-weight: 600;
     line-height: 1.125;
     font-size: 5rem;
+  }
+
+  .pulse-button__wrapper {
+    position: absolute;
+    bottom: 15vh;
   }
 </style>
---
+++ packages/www/src/components/PulseButton.svelte
@@ -0,0 +1,152 @@
+<!-- https://codepen.io/oliviale/pen/vPvvyr?editors=1100 -->
+<div class="button-pulse" on:click>
+  <div class="pulsing"></div>
+  <button>Sprawdź to!</button>
+</div>
+
+<style lang="scss">
+  * {
+    &:before,
+    &:after {
+      content: "";
+      position: absolute;
+    }
+  }
+
+  button {
+    background: transparent;
+    color: #fff;
+    border: 3px solid #fff;
+    border-radius: 50px;
+    padding: 0.8rem 2rem;
+    font: 24px "Margarine", sans-serif;
+    outline: none;
+    cursor: pointer;
+    position: relative;
+    transition: 0.2s ease-in-out;
+    letter-spacing: 2px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+  }
+
+  .button-pulse {
+    display: inline-block;
+    position: relative;
+    width: 230px;
+    height: 65px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      animation: jittery 2s infinite;
+    }
+  }
+
+  .pulsing {
+    width: 99%;
+    height: 99%;
+    border-radius: 50px;
+    z-index: 1;
+    position: relative;
+
+    &::before,
+    &::after {
+      width: 100%;
+      height: 100%;
+      border: inherit;
+      top: 0;
+      left: 0;
+      z-index: 0;
+      background: #fff;
+      border-radius: inherit;
+      animation: pulsing 2.5s linear infinite;
+    }
+
+    &::after {
+      animation: pulsing1 2.5s linear infinite;
+    }
+  }
+
+  @keyframes pulsing {
+    0% {
+      opacity: 1;
+      transform: scaleY(1) scaleX(1);
+    }
+    20% {
+      opacity: 0.5;
+    }
+    70% {
+      opacity: 0.2;
+      transform: scaleY(1.8) scaleX(1.4);
+    }
+    80% {
+      opacity: 0;
+      transform: scaleY(1.8) scaleX(1.4);
+    }
+    90% {
+      opacity: 0;
+      transform: scaleY(1) scaleX(1);
+    }
+  }
+
+  @keyframes pulsing1 {
+    0% {
+      opacity: 1;
+      transform: scaleY(1) scaleX(1);
+    }
+    20% {
+      opacity: 0.5;
+    }
+    70% {
+      opacity: 0.2;
+      transform: scaleY(1.3) scaleX(1.15);
+    }
+    80% {
+      opacity: 0;
+      transform: scaleY(1.3) scaleX(1.15);
+    }
+    90% {
+      opacity: 0;
+      transform: scaleY(1) scaleX(1);
+    }
+  }
+
+  @keyframes jittery {
+    10% {
+      transform: scale(1);
+    }
+
+    20% {
+      transform: scale(0.9);
+    }
+
+    30% {
+      transform: scale(1.15);
+    }
+
+    40% {
+      transform: scale(1.15) rotate(-5deg);
+    }
+
+    50% {
+      transform: scale(1.15) rotate(5deg);
+    }
+
+    60% {
+      transform: scale(1.15) rotate(-3deg);
+    }
+
+    70% {
+      transform: scale(1.15) rotate(2deg);
+    }
+
+    90% {
+      transform: scale(1.15) rotate(0);
+    }
+  }
+</style>
