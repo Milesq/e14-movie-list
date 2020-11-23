4229738edb55e4bd24ab3d5da0ec89c3e4776356
Milesq <wmilosz88@gmail.com>
2020-11-23 19:42:53 +0100

    feat: add parameters service

--- packages/api/src/graphql/queries/movie.js
+++ packages/api/src/graphql/queries/movie.js
@@ -11,7 +11,7 @@ const schema = gql`
 
   enum MovieField {
     TITLE
-    GENRE
+    RATING
     YEAR
   }
 
@@ -21,6 +21,31 @@ const schema = gql`
   }
 `
 
-const resolver = () => Movie.find()
+const resolver = async (parent, { limit, sortBy, order = 'ASC' }) => {
+  const movies = await Movie.find()
+  const orderFn = {
+    ASC: (a, b) => a > b,
+    DESC: (a, b) => a < b,
+  }
+
+  if (sortBy) {
+    movies.sort((first, second) => {
+      const a = first[sortBy.toLocaleLowerCase()].toString().toLocaleLowerCase()
+      const b = second[sortBy.toLocaleLowerCase()]
+        .toString()
+        .toLocaleLowerCase()
+
+      if (orderFn[order === 'ASC' ? 'DESC' : 'ASC'](a, b)) return -1
+      if (orderFn[order](a, b)) return 1
+      return 0
+    })
+  }
+
+  if (limit) {
+    return movies.slice(0, limit)
+  }
+
+  return movies
+}
 
 module.exports = { schema, resolver }
--- packages/api/tests/list-movies.spec.js
+++ packages/api/tests/list-movies.spec.js
@@ -47,84 +47,84 @@ describe('List getting', () => {
 
     expect(res.data.movie[0].title).toBe(content[0].title)
   })
-})
 
-describe('movies with parameters', () => {
-  it('`limit` works', async () => {
-    const content = [
-      {
-        title: 'Phantom Meance',
-        genre: 'Sci-Fi',
-        production: 1999,
-        rating: 4,
-      },
-      {
-        title: 'Attack of the Clone',
-        genre: 'Sci-Fi',
-        production: 2002,
-        rating: 4,
-      },
-      {
-        title: 'Revenge of the Sith',
-        genre: 'Sci-Fi',
-        production: 2005,
-        rating: 5,
-      },
-    ]
+  describe('with parameters', () => {
+    it('`limit` works', async () => {
+      const content = [
+        {
+          title: 'Phantom Meance',
+          genre: 'Sci-Fi',
+          production: 1999,
+          rating: 4,
+        },
+        {
+          title: 'Attack of the Clone',
+          genre: 'Sci-Fi',
+          production: 2002,
+          rating: 4,
+        },
+        {
+          title: 'Revenge of the Sith',
+          genre: 'Sci-Fi',
+          production: 2005,
+          rating: 5,
+        },
+      ]
 
-    mockingoose(Movie).toReturn(content)
+      mockingoose(Movie).toReturn(content)
 
-    const { query } = createTestClient(server)
+      const { query } = createTestClient(server)
 
-    const moviesQuery = gql`
-      query {
-        movie(limit: 2) {
-          title
+      const moviesQuery = gql`
+        query {
+          movie(limit: 2) {
+            title
+          }
         }
-      }
-    `
+      `
 
-    const res = await query({ query: moviesQuery })
+      const res = await query({ query: moviesQuery })
 
-    expect(res.data.movie.length).toBe(2)
-  })
+      expect(res.data.movie.length).toBe(2)
+    })
 
-  it('`sortBy` works', async () => {
-    const { query } = createTestClient(server)
+    it('`sortBy` works', async () => {
+      const { query } = createTestClient(server)
 
-    const moviesQuery = gql`
-      query {
-        movie(sortBy: YEAR) {
-          year
+      const moviesQuery = gql`
+        query {
+          movie(sortBy: YEAR) {
+            year
+          }
         }
-      }
-    `
+      `
 
-    let {
-      data: { movie },
-    } = await query({ query: moviesQuery })
-    movie = movie.map(pick('year'))
+      let {
+        data: { movie },
+      } = await query({ query: moviesQuery })
+      movie = movie.map(pick('year'))
 
-    expect(() => movie.reduce(createisMonotonicReducer())).not.toThrow('')
-  })
+      expect(() => movie.reduce(createisMonotonicReducer())).not.toThrow('')
+    })
 
-  it('`sortBy` descending works', async () => {
-    const { query } = createTestClient(server)
+    it('`sortBy` descending works', async () => {
+      const { query } = createTestClient(server)
 
-    const moviesQuery = gql`
-      query {
-        movie(sortBy: YEAR, order: DESC) {
-          year
+      const moviesQuery = gql`
+        query {
+          movie(sortBy: YEAR, order: DESC) {
+            year
+          }
         }
-      }
-    `
+      `
 
-    let {
-      data: { movie },
-    } = await query({ query: moviesQuery })
-    movie = movie.map(pick('year'))
+      let {
+        data: { movie },
+      } = await query({ query: moviesQuery })
+      movie = movie.map(pick('year'))
 
-    expect(() => movie.reduce(createisMonotonicReducer(desc))).not.toThrow('')
+      expect(() => movie.reduce(createisMonotonicReducer(desc))).not.toThrow('')
+    })
   })
 })
 
