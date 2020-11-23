16516b083f965e9e30781a0b31d4a009e84c15ac
Milesq <wmilosz88@gmail.com>
2020-11-23 01:39:37 +0100

    feat: add basic structure

--- packages/www/src/App.svelte
+++ packages/www/src/App.svelte
@@ -1,12 +1,46 @@
 <main>
-  <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
+  <section class="section has-text-centered hero is-fullheight py-0 px-0">
+    <div class="hero-body is-flex is-flex-direction-column">
+        <h1 class="my-6">Filmoteka</h1>
+        <h2 class="subtitle is-capitalized is-size-4">Największa baza filmów!</h2>
     </div>
   </section>
+
+  <MovieOptions />
+
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Filmoteka - Polska baza baza filmów &copy; 2020
+      </p>
+    </div>
+  </footer>
 </main>
+
+<script>
+  import MovieOptions from './components/MovieOptions.svelte'
+</script>
+
+<style lang="scss">
+  @use 'sass:color';
+
+  .hero-body {
+    background-image: url(/frame.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-color: rgba(0, 0, 0, 0.4);
+    background-blend-mode: multiply;
+    background-attachment: fixed;
+
+    * {
+      color: color.invert(#222);
+    }
+  }
+
+  h1 {
+    font-weight: 600;
+    line-height: 1.125;
+    font-size: 5rem;
+  }
+</style>
---
+++ packages/www/src/components/MovieOptions.svelte
@@ -0,0 +1,38 @@
+<div class="movies container my-6 px-6-touch">
+  <div class="tile is-ancestor">
+    <div class="tile">
+      <div class="tile">
+        <div class="tile is-parent is-vertical">
+          <article class="tile is-child notification is-primary">
+            <p class="title">Dodawanie Filmów</p>
+            <p class="subtitle">Twojego filmu nie ma w naszej bazie? Dodaj go!</p>
+          </article>
+          <article class="tile is-child notification is-info">
+            <p class="title">Wyszukaj</p>
+            <p class="subtitle">Przeglądaj filmy po kategoriach!</p>
+          </article>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-warning">
+            <p class="title">Film Dnia</p>
+            <p class="subtitle">Zapoznaj się z filmem doceniomym przez krytyków!</p>
+            <figure class="image is-4by3">
+              <img src="https://picsum.photos/400/300" alt="">
+            </figure>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .movies {
+    @media screen and (min-width: 550px) and (max-width: 1024px) {
+      margin: auto 2rem;
+    }
+    @media screen and (max-width: 549px) {
+      margin: auto 1rem;
+    }
+  }
+</style>
