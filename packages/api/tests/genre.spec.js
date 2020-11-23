658edb63e97f5ccd3655025498e3669ba0dfad40
Milesq <wmilosz88@gmail.com>
2020-11-23 19:51:12 +0100

    test: add missing genre tests

---
+++ packages/api/tests/genre.spec.js
@@ -0,0 +1,62 @@
+const { default: mockingoose } = require('mockingoose')
+const { gql } = require('apollo-server')
+const { createTestClient } = require('apollo-server-testing')
+const server = require('../src/server')
+const Movie = require('../src/model/movie')
+
+const content = [
+  {
+    title: 'Phantom Meance',
+    genre: 'Sci-Fi',
+  },
+  {
+    title: 'Attack of the Clone',
+    genre: 'Adventure',
+  },
+]
+
+describe('List getting', () => {
+  beforeEach(() => {
+    mockingoose(Movie).toReturn(content)
+  })
+
+  afterEach(mockingoose.resetAll)
+
+  it('works', async () => {
+    const { query } = createTestClient(server)
+
+    const moviesQuery = gql`
+      query {
+        genre {
+          title
+          movies {
+            title
+          }
+        }
+      }
+    `
+
+    const res = await query({ query: moviesQuery })
+
+    const template = [
+      {
+        title: 'Sci-Fi',
+        movies: [
+          {
+            title: 'Phantom Meance',
+          },
+        ],
+      },
+      {
+        title: 'Adventure',
+        movies: [
+          {
+            title: 'Attack of the Clone',
+          },
+        ],
+      },
+    ]
+
+    expect(res.data.genre).toEqual(template)
+  })
+})
