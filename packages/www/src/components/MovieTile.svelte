d2e6d2d7c9b3f90e016b0f93df36c87455db76cc
Milesq <wmilosz88@gmail.com>
2020-11-24 11:28:49 +0100

    feat: show every movie

---
+++ packages/www/src/components/MovieTile.svelte
@@ -0,0 +1,37 @@
+<div class={className}>
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img {src} alt="Movie poster">
+      <figcaption>
+        ok
+      </figcaption>
+    </figure>
+  </div>
+  <div class="card-content">
+    <p class="title is-4">{title}</p>
+    <p class="subtitle is-6">{awards}</p>
+
+    <div class="content">
+      {shortPlot}
+      <hr>
+      <span class="is-pulled-left">{rating}/5</span>
+      <span class="is-pulled-right">{year}</span>
+    </div>
+  </div>
+</div>
+
+<script>
+  const emptyImage =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png';
+
+  export let className = '';
+  export let img = '';
+  export let title;
+  export let year;
+  export let plot;
+  export let awards;
+  export let rating = 0;
+
+  $: src = img || emptyImage;
+  $: shortPlot = plot.slice(0, 150) + (plot.length > 150? '...' : '');
+</script>
--- packages/www/src/pages/ShowMovies.svelte
+++ packages/www/src/pages/ShowMovies.svelte
@@ -1,9 +1,44 @@
-<div>ok</div>
+<MainNav />
+<div class="container pt-6">
+  <main class="columns">
+    {#each movies as {poster, title, year, plot, awards, rating}}
+      <MovieTile
+        img={poster}
+        {title}
+        {year}
+        {plot}
+        {rating}
+        {awards}
+        className="card pb-3 column is-3"
+      />
+    {/each}
+  </main>
+</div>
 
 <script>
   import { onMount } from 'svelte';
+  import MainNav from '../components/MainNav.svelte';
+  import MovieTile from '../components/MovieTile.svelte';
+  import req, { gql } from '../utils/graphqlClient';
 
   export let params = {};
 
-  onMount(() => console.log(params));
+  let movies = [];
+
+  onMount(() => {
+    req(gql`
+      query {
+        movie {
+          title
+          year
+          rating
+          plot
+          poster
+          awards
+        }
+      }
+    `).then(({ movie }) => {
+      movies = movie;
+    });
+  });
 </script>
